f313c6a0605ae0a51b82d47d5befffedd2030bab
mordaroso <mordaroso@gmail.com>
2010-08-27 11:51:56 +0200

    correctly chain scope_by methods onto existing scopes

--- lib/simplificator_filter/scope_logic/scope_logic.rb
+++ lib/simplificator_filter/scope_logic/scope_logic.rb
@@ -1,34 +1,8 @@
 module ScopeLogic
 
-  module ClassMethods
-
-    # creates a new filter definition if a block is passed
-    # if no block is passed it returns the filter definition object
-    def scope_definition scope_strategy
-      definition_name  = "#{scope_strategy}_definition"  # 'filter_definition'
-      definition_class = "#{scope_strategy.to_s.capitalize}able::#{scope_strategy.to_s.capitalize}Definition".constantize # Filterable::FilterDefinition
-
-      if block_given?
-        yield instance_variable_get("@#{definition_name}") || instance_variable_set("@#{definition_name}", definition_class.new(self))
-      else
-        instance_variable_get("@#{definition_name}")
-      end
-    end
-
-    def default_scopes_for_all_attributes scope_strategy
-      send "default_#{scope_strategy.to_s.pluralize}_for_attributes", *self.column_names.reject {|c| c == self.primary_key.to_s }
-    end
-
-    def default_scopes_for_attributes scope_strategy, *attributes
-      definition_name  = "#{scope_strategy}_definition"  # 'filter_definition'
-      definition_class = "#{scope_strategy.to_s.capitalize}able::#{scope_strategy.to_s.capitalize}Definition".constantize # Filterable::FilterDefinition
-
-      instance_variable_get("@#{definition_name}") || instance_variable_set("@#{definition_name}", definition_class.new(self))
-      attributes.each do |attribute|
-        instance_variable_get("@#{definition_name}").send(attribute)
-      end
-    end
-
+  # The method scoped by is needed in ActiveRecord and in the Scope Class.
+  # The Scope Class needs the method to correctly chain scoped_by onto an already executed scope
+  module ScopeBy
     # @return NamedScope (Rails 2) or Scope (Rails 3)
     def scoped_by name, parameters
       if parameters
@@ -45,6 +19,51 @@ module ScopeLogic
         scoped({})
       end
     end
+  end
 
+  module ClassMethods
+    include ScopeLogic::ScopeBy
+
+    # creates a new filter definition if a block is passed
+    # if no block is passed it returns the filter definition object
+    def scope_definition scope_strategy
+      definition_name  = "#{scope_strategy}_definition"  # 'filter_definition'
+      scope_name       = "#{scope_strategy}_by"          # 'filter_by'
+      definition_class = "#{scope_strategy.to_s.capitalize}able::#{scope_strategy.to_s.capitalize}Definition".constantize # Filterable::FilterDefinition
+
+      setup_scope_class scope_name, definition_name
+
+      if block_given?
+        yield instance_variable_get("@#{definition_name}") || instance_variable_set("@#{definition_name}", definition_class.new(self))
+      else
+        instance_variable_get("@#{definition_name}")
+      end
+    end
+
+    # includes ScopeBy method and defines methods for strategies (e.g. filter_by) to chain them correctly onto an already executed scope
+    # Excample: FooBar.available.filter_by(:name => 'bar')
+    def setup_scope_class(scope_name, definition_name)
+      ActiveRecord::NamedScope::Scope.instance_eval do
+        include ScopeLogic::ScopeBy
+
+        define_method scope_name do |*args|
+          scoped_by(definition_name, *args)
+        end
+      end
+    end
+
+    def default_scopes_for_all_attributes scope_strategy
+      send "default_#{scope_strategy.to_s.pluralize}_for_attributes", *self.column_names.reject {|c| c == self.primary_key.to_s }
+    end
+
+    def default_scopes_for_attributes scope_strategy, *attributes
+      definition_name  = "#{scope_strategy}_definition"  # 'filter_definition'
+      definition_class = "#{scope_strategy.to_s.capitalize}able::#{scope_strategy.to_s.capitalize}Definition".constantize # Filterable::FilterDefinition
+
+      instance_variable_get("@#{definition_name}") || instance_variable_set("@#{definition_name}", definition_class.new(self))
+      attributes.each do |attribute|
+        instance_variable_get("@#{definition_name}").send(attribute)
+      end
+    end
   end
 end
