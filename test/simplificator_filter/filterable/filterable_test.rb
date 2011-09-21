117f37f0c166e06e81c9c318a47ae431183681ca
Krzysztof Sakwerda <krzysztof.sakwerda@simplificator.com>
2011-09-21 20:57:02 +0200

    Descriptive names for classes in tests

--- test/simplificator_filter/filterable/filterable_test.rb
+++ test/simplificator_filter/filterable/filterable_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Foo2 < ActiveRecord::Base
+class OrdersWithFilters2 < ActiveRecord::Base
   set_table_name 'orders'
 
   include Filterable
@@ -20,7 +20,7 @@ class TestFilterable < Test::Unit::TestCase
     context "filter_definition" do
 
       should "have a filter definition" do
-        assert_instance_of Filterable::FilterDefinition, Foo2.filter_definition
+        assert_instance_of Filterable::FilterDefinition, OrdersWithFilters2.filter_definition
       end
 
     end
@@ -29,62 +29,62 @@ class TestFilterable < Test::Unit::TestCase
 
       setup do
         names = ['magic carpet', 'red carpet', 'water bottle', 'whisky bottle']
-        0.upto(3) {|i| Foo2.create(:price => (i+1)*10, :product_name => names[i], :purchased_at => i.days.ago.to_date) }
+        0.upto(3) {|i| OrdersWithFilters2.create(:price => (i+1)*10, :product_name => names[i], :purchased_at => i.days.ago.to_date) }
       end
 
 
       should "skip filter scope" do
-        carpets = Foo2.filter_by(nil)
+        carpets = OrdersWithFilters2.filter_by(nil)
         assert_equal 4, carpets.size
       end
 
       should "skip filter scope 2" do
-        carpets = Foo2.filter_by(nil).cheap
+        carpets = OrdersWithFilters2.filter_by(nil).cheap
         assert_equal 1, carpets.size
       end
 
       should "find all carpets" do
-        carpets = Foo2.filter_by(:fuzzy_name => 'carpet')
+        carpets = OrdersWithFilters2.filter_by(:fuzzy_name => 'carpet')
         assert_equal 2, carpets.size
       end
 
       should "find cheaper carpert" do
-        carpets = Foo2.filter_by(:fuzzy_name => 'carpet').cheap
+        carpets = OrdersWithFilters2.filter_by(:fuzzy_name => 'carpet').cheap
         assert_equal 1, carpets.size
         assert_equal 'magic carpet', carpets.first.product_name
       end
 
       should "find cheaper carpert too" do
-        carpets = Foo2.cheap.filter_by(:fuzzy_name => 'carpet')
+        carpets = OrdersWithFilters2.cheap.filter_by(:fuzzy_name => 'carpet')
         assert_equal 1, carpets.size
         assert_equal 'magic carpet', carpets.first.product_name
       end
 
       should "find expensiver carpet" do
-        carpets = Foo2.filter_by(:fuzzy_name => 'carpet', :price_range => '15 - 45')
+        carpets = OrdersWithFilters2.filter_by(:fuzzy_name => 'carpet', :price_range => '15 - 45')
         assert_equal 1, carpets.size
         assert_equal 'red carpet', carpets.first.product_name
       end
 
       should "find red carpet" do
-        carpets = Foo2.filter_by(:fuzzy_name => 'carpet', :price_range => '15 - 45', :purchased_at => 1.day.ago.to_date)
+        carpets = OrdersWithFilters2.filter_by(:fuzzy_name => 'carpet', :price_range => '15 - 45', :purchased_at => 1.day.ago.to_date)
         assert_equal 1, carpets.size
         assert_equal 'red carpet', carpets.first.product_name
       end
 
       should "find water bottle" do
-        orders = Foo2.filter_by(:price_range => '15 - 45').all(:conditions => {:product_name => 'water bottle'})
+        orders = OrdersWithFilters2.filter_by(:price_range => '15 - 45').all(:conditions => {:product_name => 'water bottle'})
         assert_equal 1, orders.size
         assert_equal 'water bottle', orders.first.product_name
       end
 
       should "accept blank for fuzzy name" do
-        orders = Foo2.filter_by(:fuzzy_name => '')
+        orders = OrdersWithFilters2.filter_by(:fuzzy_name => '')
         assert_not_equal({}, orders.filters)
       end
 
       should "not accept blank for price range" do
-        orders = Foo2.filter_by(:price_range => '')
+        orders = OrdersWithFilters2.filter_by(:price_range => '')
         assert_equal({}, orders.filters)
       end
     end
